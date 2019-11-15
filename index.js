f7d847f7131f88b1ee20add13131c5934fe8e072
Deysol <Whatthepolo@gmail.com>
2019-11-15 23:35:16 +1100

    Update index.js

--- index.js
+++ index.js
@@ -121,6 +121,7 @@ client.on('message', message => {
 });
 
 // Auto-Replys
+
 client.on('message', message=> {
     if (message.isMentioned(client.users.get('579108689852760087'))) {
     message.delete(100);
@@ -131,6 +132,12 @@ client.on('message', message=> {
 
 
 // Other
+client.on('message', message=> {
+    if (message.isMentioned(client.users.get('574090926985576448'))) {
+    message.delete(100);
+    message.reply('Do not ping Swofty! Multiple offences will result in a permanent mute')
+  }
+});
 client.on('message', message => {
   if (message.content === 'Hi') {
     message.reply('Hey!');
